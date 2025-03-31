bdd64283b3f5f917e573f53ebb4c43f836f4dc50
AalokMoliya <moliyaaalok1312@gmail.com>
2025-03-31 17:09:44 +0530

    Update add_round_key.v

--- add_round_key.v
+++ add_round_key.v
@@ -1,9 +1,5 @@
-module add_round_key(input[127:0] state_in,input[127:0] main_key,input [3:0] round,output [127:0] state_out);
-
-  //generation of expanded key for perticular round
-  wire [127:0] round_key;
-  key_expansion a(.key(main_key),.round(round),.op_key(round_key));
+module add_round_key(input[127:0] state_in,input[127:0] key,output [127:0] state_out);
   
   //xor of expanded key with input state
-  assign state_out=state_in^round_key;
+  assign state_out=state_in^key;
 endmodule
