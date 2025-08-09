3959e6ceff4b19a99d2c48f64b86d34cacc53648
SHUDHANSHU-BHADANA <shudhanshubhadana1234@gmail.com>
2025-08-09 17:27:21 +0530

    Update aes_pipelined.v

--- aes_pipelined.v
+++ aes_pipelined.v
@@ -166,10 +166,10 @@ module mixcolumns (
     generate
         for (i = 0; i < 4; i = i + 1) begin : mix_column
             // Extract column i 
-            wire [7:0] b0 = state_in[127-32*i -: 8];  
-            wire [7:0] b1 = state_in[127-32*i-8 -: 8]; 
-            wire [7:0] b2 = state_in[127-32*i-16 -: 8]; 
-            wire [7:0] b3 = state_in[127-32*i-24 -: 8]; 
+            wire [7:0] b0 = state_in[(127-32*i) : (127-32*i)-7];  
+            wire [7:0] b1 = state_in[(127-32*i)-8 : (127-32*i)-15];  
+            wire [7:0] b2 = state_in[(127-32*i)-16 : (127-32*i)-23];  
+            wire [7:0] b3 = state_in[(127-32*i)-24 : (127-32*i)-31]; 
             
             // MixColumns transformation
             wire [7:0] new_b0 = gf_mul2(b0) ^ gf_mul3(b1) ^ b2 ^ b3;
@@ -178,10 +178,10 @@ module mixcolumns (
             wire [7:0] new_b3 = gf_mul3(b0) ^ b1 ^ b2 ^ gf_mul2(b3);
             
             // Assign output (column-major order)
-            assign state_out[127-32*i -: 8] = new_b0;
-            assign state_out[127-32*i-8 -: 8] = new_b1;
-            assign state_out[127-32*i-16 -: 8] = new_b2;
-            assign state_out[127-32*i-24 -: 8] = new_b3;
+            assign state_out[(127-32*i) : (127-32*i)-7] = new_b0;
+            assign state_out[(127-32*i)-8 : (127-32*i)-15] = new_b1;
+            assign state_out[(127-32*i)-16 : (127-32*i)-23] = new_b2;
+            assign state_out[(127-32*i)-24 : (127-32*i)-31] = new_b3;
         end
     endgenerate
 endmodule
