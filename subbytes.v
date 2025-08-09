298ac3e30d12fb573abdbdb0207b7d0e07f15d47
Mayank Gangwar <mayankgangwar84382@gmail.com>
2025-08-09 18:10:22 +0530

    Update subbytes.v

--- subbytes.v
+++ subbytes.v
@@ -3,16 +3,14 @@ module aes_sub_bytes(
     output [127:0] state_out
 );
     
-    wire [7:0] sbox_out[15:0];
     
     genvar i;
     generate
         for (i = 0; i < 16; i = i + 1) begin : sub_bytes_loop
           aes_sbox sbox_inst (
-                .data_in(state_in[(i*8) +: 8]),
-                .data_out(sbox_out[i])
+              .data_in(state_in[(8*i)+7: 8*i]),
+              .data_out(state_out[(8*i)+7: 8*i])
             );
-            assign state_out[(i*8) +: 8] = sbox_out[i];
         end
     endgenerate
 
