2ccee8de0da7770409aacdaa96b17bd1678d001a
AalokMoliya <moliyaaalok1312@gmail.com>
2025-08-09 18:02:37 +0530

    Update mixcolumn.v

--- mixcolumn.v
+++ mixcolumn.v
@@ -6,12 +6,12 @@ module mixcolumns (
     function [7:0] gf_mul2;
         input [7:0] b;
         begin
-            gf_mul2 = {b[6:0], 1'b0} ^ (8'h1b & {8{b[7]}});
+            gf_mul2 = {b[6:0], 1'b0} ^ (8'h1b & {8{b[7]}});  //exor with 0x1b to handle overflow
         end
     endfunction
 
     function [7:0] gf_mul3;
-        input [7:0] b;                           // to multiply by 3 , mul by two and then add once (using exor fn)
+        input [7:0] b;
         begin
             gf_mul3 = gf_mul2(b) ^ b;
         end
@@ -22,10 +22,10 @@ module mixcolumns (
     generate
         for (i = 0; i < 4; i = i + 1) begin : mix_column
             // Extract column i 
-            wire [7:0] b0 = state_in[127-32*i -: 8];  
-            wire [7:0] b1 = state_in[127-32*i-8 -: 8]; 
-            wire [7:0] b2 = state_in[127-32*i-16 -: 8]; 
-            wire [7:0] b3 = state_in[127-32*i-24 -: 8]; 
+            wire [7:0] b0 = state_in[(127-32*i) : (127-32*i)-7];  
+            wire [7:0] b1 = state_in[(127-32*i)-8 : (127-32*i)-15];  
+            wire [7:0] b2 = state_in[(127-32*i)-16 : (127-32*i)-23];  
+            wire [7:0] b3 = state_in[(127-32*i)-24 : (127-32*i)-31]; 
             
             // MixColumns transformation
             wire [7:0] new_b0 = gf_mul2(b0) ^ gf_mul3(b1) ^ b2 ^ b3;
@@ -34,10 +34,10 @@ module mixcolumns (
             wire [7:0] new_b3 = gf_mul3(b0) ^ b1 ^ b2 ^ gf_mul2(b3);
             
             // Assign output (column-major order)
-            assign state_out[127-32*i -: 8] = new_b0;
-            assign state_out[127-32*i-8 -: 8] = new_b1;
-            assign state_out[127-32*i-16 -: 8] = new_b2;
-            assign state_out[127-32*i-24 -: 8] = new_b3;
+            assign state_out[(127-32*i) : (127-32*i)-7] = new_b0;
+            assign state_out[(127-32*i)-8 : (127-32*i)-15] = new_b1;
+            assign state_out[(127-32*i)-16 : (127-32*i)-23] = new_b2;
+            assign state_out[(127-32*i)-24 : (127-32*i)-31] = new_b3;
         end
     endgenerate
 endmodule
